68534fe43bdd5c4540e1f25fe61329d373ac0e7d
Guillaume <guillaume.rebourgeon@hotmail.fr>
2024-10-25 15:48:54 +0200

    Tableau comme avant

--- public/js/tables.js
+++ public/js/tables.js
@@ -35,3 +35,29 @@ document.addEventListener("DOMContentLoaded", function () {
     setInterval(changePhrase, 30000); // Change toutes les 30 secondes
 });
 
+// const texte = 'Savourez des arômes authentiques des bières artisanales du monde entier.';
+// const blanc = '          '; // Espace entre chaque répétition du texte
+// const nbTexte = 4; // Nombre de répétitions du texte
+
+// const msg = (texte + blanc).repeat(nbTexte); // Répéter le texte
+
+// // Sélectionner l'élément où le texte va défiler
+// const scrollingTextElement = document.getElementById('changing-text-continent');
+
+//  // Fonction pour mettre le texte dans l'élément et redémarrer l'animation
+//  function updateText(newText) {
+//     scrollingTextElement.textContent = newText;
+
+//     // Réinitialiser l'animation en utilisant JavaScript
+//     scrollingTextElement.style.animation = 'none'; // Stopper l'animation
+//     scrollingTextElement.offsetHeight; // Trigger reflow pour redémarrer l'animation
+//     scrollingTextElement.style.animation = ''; // Relancer l'animation
+// }
+
+// // Initialiser avec le texte
+// updateText(msg);
+
+// Si vous voulez changer le texte après un certain délai, vous pouvez faire ceci :
+// setInterval(() => {
+//     updateText(msg); // Vous pouvez changer le texte ici si nécessaire
+// }, 30000); // Par exemple, toutes les 30 secondes
